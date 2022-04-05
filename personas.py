0ba29b8057987566f230496ae1aa83783bb104bc
AngelPuent3 <99633191+AngelPuent3@users.noreply.github.com>
2022-04-05 15:05:00 -0500

    Delete personas.py

--- personas.py
+++
@@ -1,9 +0,0 @@
-class Persona:
-    def __init__(self):
-        self.nombre = str
-        self.RFC = str
-        self.tipoPaga = int
-        self.paga = int
-
-    def calcularSalario(self):
-        pass
